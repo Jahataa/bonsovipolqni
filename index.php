f9c6caa040102b3e267500bf67a7e65f3a90683c
bladespinner <bladespinner@gmail.com>
2014-01-03 22:27:11 +0200

    More files moved.

--- index.php
+++ index.php
@@ -1,17 +1,85 @@
-﻿<div class="mastar">
-<?php require ("header.php");
-?>
-    <div class="content">
-        <p>
-        	<strong>1 Стара букова гора, на 15 мин от София, почивка, спокойствие, отмора от прашния град.<br />
-             Всичко това на хижа "Бонсови поляни" ..... Сватби, кръщенета, празници, рождени дни...... <br />
-             Вкусна кухня, кристален въздух..... не е нужна реклама, просто елате един път .<br />
-             Ако е студено и мъгливо ще усетите старата печка и миризмата на дърва.<br />
-             Ако е слънчево и топло ще походите из старите гори да си поговорите с тях.<br />
-             Диагностика на биоскенер, полезни съвети и природни лечебни продукти.............ВСЯКА събота!!<br /> 
-            </strong>
-        </p>
+<html>
+  <head>
+    <title>Bonsovi polqni</title>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <script src="resources/jquery.js" type="text/javascript"></script>
+    <script src="resources/jquery.carousel.js" type="text/javascript"></script>
+    <script>
+      $(document).ready()
+      {
+        $(".carousel").Carousel();
+      }
+    </script>
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <nav class="tabs">
+          <a class="tab selected" href="#">
+            <img />
+            <b>Bonsovi Polqni</b>
+          </a>
+          <a class="tab" href="#">
+            <img />
+            <b>Bilkis</b>
+          </a>
+        </nav>
+        <nav class="menu">
+          <ul>
+            <li><a href="#">Link1</a></li>
+            <li><a href="#">Link2</a></li>
+            <li><a href="#">Link3</a></li>
+            <li><a href="#">Link4</a></li>
+            <li><a href="#">Link5</a></li>
+          </ul>
+        </nav>
+      </header>
+      <article>
+        <h1>Bonsovi Polqni Home Page Title</h1>
+        <p>This is some content , like it or not.</p>
+        <figure class="carousel">
+        
+          <div class="slide">
+            <img/>
+            <div class="r-col">
+              <h2>Product1</h2>
+              <p>
+                This is some long long long long long long long long long long long long long long long
+                long long long long long long long long long long long long long long long product description
+              </p>
+              <div class="price">
+                15.44 лв.
+              </div>
+            </div>
+          </div>
+          
+          <div class="slide">
+            <img/>
+            <div class="r-col">
+              <h2>Product2</h2>
+              <p>
+                This is some long long long long long long long long long long long long long long long
+                long long long long long long long long long long long long long long long product description
+              </p>
+              <div class="price">
+                14.44 лв.
+              </div>
+            </div>
+          </div>
+          
+        </figure>
+        <p>This is some additional content , like it or not.</p>
+      <article>
+      <footer>
+        <nav class="footerNav">
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Disclaimer</a></li>
+            <li>© Bonsovi Polqni</li>
+            <li>Made by <abbr title="Nasko Nasko Misho">?NNM?</abbr></li>
+          </ul>
+        </nav>
+      </footer>
     </div>
-</div>
-<script src="js/jquery-1.10.2.min.js"></script>
-<script src="js/lightbox-2.6.min.js"></script>
+  </body>
+</html>
