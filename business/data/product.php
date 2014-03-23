94a434365e36865a1297cb8aba33a183a0377fda
bladespinner <bladespinner@gmail.com>
2014-03-24 01:03:57 +0200

    agdsgasdgsdg

--- business/data/product.php
+++ business/data/product.php
@@ -78,19 +78,88 @@
 
 	class WebsiteRepository
 	{
-		function GetWebsite()
+		function GetWebsites()
 		{
+			$websites = [];
+			$mysqli = new mysqli($SQLSERVER, $SQLACC, $SQLPASS, $SQLSCHEMA);
+			if ($mysqli->connect_error) 
+			{
+				die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
+			}
 
+			if($result = $mysqli->query("SELECT * FROM `website`"))
+			{
+				if($result->num_rows > 0) 
+				{
+					while($row = $result->fetch_assoc()) 
+					{
+						$website = new Website();
+
+						$website->$id = $row['id'];
+						$website->$id = $row['websiteName'];
+						$website->$id = $row['websiteUrl'];
+						$website->$id = $row['imageUrl'];
+
+						$websites[] = $website;
+					}
+				}
+
+				$result -> close();
+			}
+
+			$mysqli->close();
+			return $websites;
 		}
 
-		function GetWebsite(Website $websiteId)
+		function GetWebsite($websiteId)
 		{
+			$website = NULL;
+			$mysqli = new mysqli($SQLSERVER, $SQLACC, $SQLPASS, $SQLSCHEMA);
+			if ($mysqli->connect_error) 
+			{
+				die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
+			}
 
+			if($result = $mysqli->query("SELECT * FROM `website` WHERE id='".$websiteId."'"))
+			{
+				if($result->num_rows > 0) 
+				{
+					while($row = $result->fetch_assoc()) 
+					{
+						$website = new Website();
+						
+						$website->$id = $row['id'];
+						$website->$name = $row['websiteName'];
+						$website->$url = $row['websiteUrl'];
+						$website->$imageUrl = $row['imageUrl'];
+					}
+				}
+
+				$result -> close();
+			}
+
+			$mysqli->close();
+			return $website;
 		}
 
 		function CreateWebsite(Website $website)
 		{
+			$website = NULL;
+			$mysqli = new mysqli($SQLSERVER, $SQLACC, $SQLPASS, $SQLSCHEMA);
+			if ($mysqli->connect_error) 
+			{
+				die('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error);
+			}
 
+			if($result = $mysqli->query('INSERT INTO `website`(`websiteName`, `websiteUrl`, `imageUrl`, `dateCreated`, `dateUpdated`) '.
+				'VALUES '.
+				'('.$website->$name.','.$website->$url.','.$website->$imageUrl.',NOW(),NOW())'))
+			{
+				$result -> close();
+			}
+
+			$mysqli->close();
+			return $website;
 		}
 
 		function UpdateWebsite(Website $website)
